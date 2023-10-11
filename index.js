488b90af3a0a9dce5ab5d072443bdee56356c12a
RuslanSalimov <mail-spam2012@yandex.ru>
2023-10-11 19:11:10 +0500

    create response

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const app = express();
 
 const port = 8000;
 
+app.all('/hello', (req, res, next) => {
+    console.log('All');
+    res.cookie('token', 'hello', {
+        secure: true,
+        domain: '/'
+    });
+    next();
+})
 app.get('/hello', (req, res) => {
-    res.send('Hello!');
+    res.status(201).send('Hello!');
+})
+app.get('/json', (req, res) => {
+    res.status(201).json({request: 'Hello'});
 })
 
 app.listen(port, () => {
